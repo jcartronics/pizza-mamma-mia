f645e70fedcf356557ddbb44608ff8a1426a7973
jcartronics <jcar-95@hotmail.com>
2023-06-30 23:50:16 -0400

    Agrega componentes - vistas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 
+//Vistas
+import { Home } from './views/Home';
+import { Pizza } from './components/Pizza';
+import { Carrito } from './views/Carrito';
+
 function App() {
   return (
     <div className="App">
