a7a6fb6108f284bcbab935e0272b3557d4a2462f
jcartronics <jcar-95@hotmail.com>
2023-07-01 00:44:41 -0400

    Agrega estructura basica Card

--- src/components/Pizzas.jsx
+++ src/components/Pizzas.jsx
@@ -1,19 +1,36 @@
 import React, { useContext } from 'react'
 import Context from '../context/Context'
+import { Button, Card, Container, Row } from 'react-bootstrap';
 
 export const Pizzas = () => {
     const {pizzas, setPizzas} = useContext(Context);
 
   return (
     <div>
-        {pizzas.map(
-            (pizza) => {
-                return (
-                    <img src={pizza.img} />
-                )
-            }
-        )
-        }
+        <Container fluid className='mx-6 my-3 p-2'>
+            <Row>
+                <Card>
+                    <Card.Img/>
+                    <Card.Body>
+                        <Card.Title>Napolitana</Card.Title>
+                        <Card.Text>Ingredientes</Card.Text>
+                        <Card.Title>Precio:</Card.Title>
+                        <Button>Ver Más</Button>
+                        <Button>Añadir</Button>
+                    </Card.Body>
+                </Card>
+            </Row>
+        </Container>
     </div>
+    // <div>
+    //     {pizzas.map(
+    //         (pizza) => {
+    //             return (
+    //                 <img src={pizza.img} />
+    //             )
+    //         }
+    //     )
+    //     }
+    // </div>
   )
 }
