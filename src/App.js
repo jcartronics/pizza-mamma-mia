45c347df9f80dfb583f5e9909c2a965a1c55b41b
jcartronics <jcar-95@hotmail.com>
2023-06-30 23:54:49 -0400

    Agrega Router y Rutas

--- src/App.js
+++ src/App.js
@@ -5,9 +5,19 @@ import { Home } from './views/Home';
 import { Pizza } from './components/Pizza';
 import { Carrito } from './views/Carrito';
 
+// React-router-dom
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/pizza/:id' element={<Pizza/>}/>
+        <Route path='/carrito' element={<Carrito/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
